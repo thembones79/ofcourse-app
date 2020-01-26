f7eacfad8201c4d14cc1b57377b4b618856bfd45
thembones79 <michal.szumnarski@gmail.com>
2020-01-26 23:00:25 +0100

    wired up server to create course function

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,28 @@
 import produce from "immer";
-import { ADD_COURSE } from "./actions";
+import {
+  ADD_COURSE_BEGIN,
+  ADD_COURSE_SUCCESS,
+  ADD_COURSE_ERROR
+} from "./actions";
 
-const initialState = { courses: [] };
+const initialState = { loading: false, courses: [], error: null };
 
 const reducer = produce((draft, action) => {
   switch (action.type) {
-    case ADD_COURSE:
-      draft.courses.push(action.payload);
+    case ADD_COURSE_BEGIN:
+      draft.loading = true;
+      draft.error = null;
       return;
+    case ADD_COURSE_SUCCESS:
+      draft.loading = false;
+      draft.courses.push(action.payload);
+      draft.error = null;
+      return;
+    case ADD_COURSE_ERROR:
+      draft.loading = false;
+      draft.error = action.error;
+      return;
+
     default:
       return;
   }
